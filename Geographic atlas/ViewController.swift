17633cb655917882c690b9a5abc27ae31b2545d6
FazilKhan7 <200107055@stu.sdu.edu.kz>
2023-05-17 14:31:18 +0600

    Initial Commit

---
+++ Geographic atlas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Geographic atlas
+//
+//  Created by Bakhtiyarov Fozilkhon on 17.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
